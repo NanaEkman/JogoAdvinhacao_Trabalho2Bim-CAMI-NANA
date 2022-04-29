d06155d6eea44e0f1016634c0dccdc8c8e9c9446
“AlexBrosso” <alexbrosso7@gmail.com>
2022-04-29 09:38:57 -0300

    Initial commit

---
+++ app/src/main/java/com/example/jogoadvinhacao_trabalho2bim_cami_nana/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.jogoadvinhacao_trabalho2bim_cami_nana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
